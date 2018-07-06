d65150be96d9e9d0cf24ad2ecab3f191ced3ecc6
Bmo1992 <brandonseahorse@gmail.com>
2018-07-06 10:00:15 -0700

    push most recent additions to 5-2 to master

--- Chapter_5/5-2.c
+++ Chapter_5/5-2.c
@@ -3,9 +3,10 @@
  *  Exercise 5.2
  *  Bmo
  */
-#include <fnctl.h>
+#include <fcntl.h>
 #include <unistd.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 /* Constant for memory buffer */
 #define BUFFER 1024
@@ -13,13 +14,31 @@
 /* Error checking function */
 void errCheck(char *errorIn)
 {
-    put(errorIn);
+    puts(errorIn);
     exit(EXIT_FAILURE);
 }
 
 int main(int argc, char **argv)
 {
-    int fd;
+    if( argc != 1 || strcmp(argv[1] == "--help"))
+    {
+        printf("\nUsage of program: \n");
+        errCheck("./5-2 <file>");
+    }
+
+    int fd, openFlags, filePerms;
+    off_t seeker;
+    ssize_t numRead; 
+    char buf[BUFFER];
+    
+
+    /* filePerms = rw-rw-rw- */
+    filePerms = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH;
+    openFlags = O_WRONLY | O_CREAT | O_APPEND;
+
+    fd = open(argv[1], openFlags, filePerms);
+    if( fd == -1 )
+        errCheck("Error opening file input file");
 
     return 0;
 }
