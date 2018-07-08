686dd342ea4f3d4a8fee7d293963b9bc10bba7c4
Bmo1992 <brandonseahorse@gmail.com>
2018-07-07 20:42:25 -0700

    Update to exercise 5-2, program not yet functional

--- Chapter_5/5-2.c
+++ Chapter_5/5-2.c
@@ -7,6 +7,7 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
+//#include <string.h>
 
 /* Constant for memory buffer */
 #define BUFFER 1024
@@ -20,25 +21,32 @@ void errCheck(char *errorIn)
 
 int main(int argc, char **argv)
 {
-    if( argc != 1 || strcmp(argv[1] == "--help"))
+    if( argc != 3 || strcmp(argv[1] == "--help"))
     {
         printf("\nUsage of program: \n");
-        errCheck("./5-2 <file>");
+        errCheck("./5-2 <file> string");
     }
 
     int fd, openFlags, filePerms;
-    off_t seeker;
-    ssize_t numRead; 
     char buf[BUFFER];
-    
+    ssize_t numRead = strlen(argv[3]); 
+ 
 
     /* filePerms = rw-rw-rw- */
     filePerms = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH;
     openFlags = O_WRONLY | O_CREAT | O_APPEND;
 
-    fd = open(argv[1], openFlags, filePerms);
-    if( fd == -1 )
+    if((fd = open(argv[1], openFlags, filePerms)) == -1)
         errCheck("Error opening file input file");
-
-    return 0;
+    
+    if(lseek(fd, 0, SEEK_SET) == -1) 
+        errCheck("Error, could not seek to beginning of file");
+    
+    if(write(fd, buf, numRead) != numRead )
+        errCheck("Error writing to file, am kill");
+    
+    if(close(fd) == -1)
+        errCheck("Can't close the file, what a shit way to end this program");
+ 
+    exit(EXIT_SUCCESS);
 }
