ee4f11f3ff187dc4df38a7a657e10325de21726f
Bmo1992 <brandonseahorse@gmail.com>
2018-07-13 10:37:21 -0700

    Updated the 5-3 exercise

--- Chapter_5/5-3.c
+++ Chapter_5/5-3.c
@@ -44,21 +44,37 @@ void checkArgs(int argc, char **argv)
 
 int main(int argc, char **argv)
 {
-    if (argc < 3 || strcmp(argv[1], "--help") == 0) {
-        printf("Usage: ./5-3 <file> numbytes [x]\n");
-        exit(EXIT_FAILURE);    
-    }    
+    if (argc < 3 || strcmp(argv[1], "--help") == 0) 
+        errCheck("Usage: ./5-3 <file> numbytes [x]");
 
-    int filePerms, openFlags;
+    int i, numBytes, inputFile, filePerms, openFlags;
+    off_t offset;
     char buf[BUFFER];
     ssize_t numRead;
+
+    numBytes = argv[2];
 
     /* rw-rw-rw- or 0666 before the umask is applied */
     filePerms = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH;
 
+    if (inputFile = open(argv[1], openFlags, filePerms) == -1)
+        errCheck("Error, failed to open file");
+
+
     if (xflag == 1) {
         /* Do not open the file with O_APPEND so we can show what happens when we don't write to a file atomically */
         openFlags = O_WRONLY | O_CREAT;
+
+        for (i = 0; i < numBytes; i++)
+        {        
+            /* Seek to the end of the file */
+            if (offset = lseek(inputFile, 0, SEEK_END) == (off_t) -1)
+                errCheck("Error seeing to the value specified as numbytes"); 
+        
+            if (write(inputFile, "x", 1) != 1) 
+                errCheck("Write using lseek failed");
+        }
+
     } 
     else if (warnflag == 1) {
         printf("Unknown option %-c\n", optopt);
@@ -67,7 +83,6 @@ int main(int argc, char **argv)
     else {
         openFlags = O_WRONLY | O_CREAT | O_APPEND;
     }
-
 
     exit(EXIT_SUCCESS);
 }
